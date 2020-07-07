941ffc10c7f8f6adcd88b0f81ea9694a688df9c4
skymir-git <seratoa@gmail.com>
2020-07-07 22:11:41 +0900

    [Add] init.

---
+++ 7.틱택토/Tr.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Tr extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Tr;
