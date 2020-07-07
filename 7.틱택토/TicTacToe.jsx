02d5d10ce321fcfaff50a879167f082c78cd2aac
skymir-git <seratoa@gmail.com>
2020-07-07 22:11:47 +0900

    [Add] init.

---
+++ 7.틱택토/TicTacToe.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class TicTacToe extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default TicTacToe;
