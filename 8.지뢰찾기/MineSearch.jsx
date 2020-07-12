a32bbfd02aaa08f0e0a09de724b28e43efdfcbd3
skymir-git <seratoa@gmail.com>
2020-07-12 13:49:20 +0900

    [Add] init.

---
+++ 8.지뢰찾기/MineSearch.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useRef, useEffect, useCallback } from 'react';
+
+const MineSearch = () => {
+    const [Mine, setMine] = useState('');
+    const search = useRef('');
+
+    useEffect(() => {
+
+    }, []);
+
+    useCallback(() => {
+
+    }, []);
+
+    return (
+        <>
+            <div>{''}</div>
+        </>
+    );
+}
+
+export default MineSearch;
